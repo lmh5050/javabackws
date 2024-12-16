0bc0d105627f780dbd7433cf306c706c478501a4
sebri-dev <145305719+sebri-dev@users.noreply.github.com>
2024-12-17 00:07:37 +0900

    beforeSecurity

---
+++ src/main/java/com/example/testtoy/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.testtoy.config;
+
+public class SecurityConfig {
+}
--- src/main/java/com/example/testtoy/config/WebMvcConfig.java
+++ src/main/java/com/example/testtoy/config/WebMvcConfig.java
@@ -5,13 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 경로 허용
-                .allowedOrigins("http://localhost:8081") // Vue.js 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowCredentials(true); // 쿠키 허용 여부
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8081")  // 요청을 허용할 출처 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // OPTIONS 추가
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
