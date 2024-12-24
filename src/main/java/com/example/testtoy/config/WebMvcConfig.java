51ff71d99b6b794e2a8114a00cd1cdf10b0b9527
lmh5050 <94948047+lmh5050@users.noreply.github.com>
2024-12-24 18:56:13 +0900

    docker setting

--- src/main/java/com/example/testtoy/config/WebMvcConfig.java
+++ src/main/java/com/example/testtoy/config/WebMvcConfig.java
@@ -11,7 +11,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081/ , http://192.168.129.191:8081/")  // 요청을 허용할 출처 설정
+                .allowedOrigins("http://localhost:8081/ , " +
+                        "http://192.168.129.191:8081/, " +
+                        "http://http://34.64.68.37:80/," +
+                        "http://http://localhost:80/" +
+                        "http://http://localhost:81/")  // 요청을 허용할 출처 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // OPTIONS 추가
                 .allowedHeaders("*")
                 .allowCredentials(true);
