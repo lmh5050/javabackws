81ad4589931a6d46e6d2af3e06fd5c466bc7e687
lmh5050 <94948047+lmh5050@users.noreply.github.com>
2025-01-13 11:25:38 +0900

    레이드 골드 체크 기능 까지 완료

--- src/main/java/com/example/testtoy/dto/WeeklyCharacterResultDto.java
+++ src/main/java/com/example/testtoy/dto/WeeklyCharacterResultDto.java
@@ -7,14 +7,14 @@ public class WeeklyCharacterResultDto {
     private String characterName;
     private String id;
     private String itemMaxLevel;
-    private String karmen;
-    private String echidna;
-    private String behimoth;
-    private String karmen4;
-    private String egir;
-    private String abrellshould;
-    private String chaos;
-    private String guardian;
-    private String epona;
-    private String guildexchange;
+    private boolean karmen;
+    private boolean echidna;
+    private boolean behimoth;
+    private boolean karmen4;
+    private boolean egir;
+    private boolean abrellshould;
+    private boolean chaos;
+    private boolean guardian;
+    private boolean epona;
+    private boolean guildexchange;
 }
