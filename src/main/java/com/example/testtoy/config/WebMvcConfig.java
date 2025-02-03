cc8f05cea2fa5367f52e39542cc118d15b8994d2
lmh5050 <94948047+lmh5050@users.noreply.github.com>
2025-02-03 19:59:38 +0900

    GCP용으로 세팅 완료 / 24시간 서버용 설정 완료

--- src/main/java/com/example/testtoy/config/WebMvcConfig.java
+++ src/main/java/com/example/testtoy/config/WebMvcConfig.java
@@ -16,8 +16,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
                         "http://34.64.68.37:80",
                         "http://localhost:80",
                         "http://localhost:81",
+                        "http://localhost:8080",
                         "http://34.47.90.90:8080",
-                        "http://34.47.90.90:80"
+                        "http://34.47.90.90:80",
+                        "http://34.47.90.90:8081",
+                        "http://10.178.0.2:8081",
+                        "http://10.178.0.2:8080",
+                        "http://10.178.0.2:80",
+                        "http://10.178.0.2:81"
+
+
                 )  // 요청을 허용할 출처 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메소드 설정
                 .allowedHeaders("*")  // 모든 헤더 허용
